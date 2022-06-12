30fca7937806a97d8ae6f6613476fc25a15d7316
Ngô Dương Kha <ngoduongkhakg2001@gmail.com>
2022-06-12 17:13:41 +0700

    fix cors

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const mongoString = process.env.DATABASE_URL;
-const nftRoutes = require('./src/routes/nft');
-const userRoutes = require('./src/routes/user');
+const nftRoutes = require("./src/routes/nft");
+const userRoutes = require("./src/routes/user");
+const cors = require("cors");
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
-database.on('error', (error) => {
+database.on("error", (error) => {
   console.log(error);
 });
 
-database.once('connected', () => {
-  console.log('Database Connected');
+database.once("connected", () => {
+  console.log("Database Connected");
 });
 const app = express();
-
+app.use(cors());
 app.use(express.json());
-app.use('/api/nft', nftRoutes);
-app.use('/api/user', userRoutes);
+app.use("/api/nft", nftRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server Started at ${PORT}`);
