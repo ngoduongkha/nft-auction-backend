1a8f4498336adf3b4a51d5d1d35b5738c2e033c7
Ngô Dương Kha <ngoduongkhakg2001@gmail.com>
2022-06-16 03:36:18 +0700

    fix bug

--- src/services/auth/auth.controller.js
+++ src/services/auth/auth.controller.js
@@ -38,7 +38,9 @@ exports.login = async (req, res) => {
 
     if (!user) {
       const newUser = new UserModel({ wallet: wallet });
+      console.log("1", newUser);
       await newUser.save();
+      console.log("2", newUser);
       user = newUser;
     }
 
